56a543455dfe3a4b98ed22ef6be6454d15941225
jaroslav.kniss <jaroslav.kniss@OBEW-PC0152.obe.access-company.com>
2016-02-17 17:14:23 +0100

    save

--- jarRig/src/com/rig/jarrig/JarrigUI.java
+++ jarRig/src/com/rig/jarrig/JarrigUI.java
@@ -2,20 +2,17 @@ package com.rig.jarrig;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("jarrig")
 public class JarrigUI extends UI {
-	
-	RigComponents components = new RigComponents();
-	@Override
-	protected void init(VaadinRequest request) {
-		setContent(components);
-	}
+
+    RigComponents components = new RigComponents();
+
+    @Override
+    protected void init(VaadinRequest request) {
+	setContent(components);
+    }
 
 }
